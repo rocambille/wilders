996afe2065f7146eb9b36a2c4b0f26420959db68
romaing <rocambille@gmail.com>
2024-03-19 14:39:31 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+import chalk from "chalk";
+
+const names = ["Audrey", "Emilie", "Alexandre", "Elodie"];
+
+console.log(chalk.red(names[0]));
+console.log(chalk.green(names[1]));
+console.log(chalk.blue(names[2]));
+console.log(chalk.yellow(names[3]));
